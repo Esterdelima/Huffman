078f1129c9df854a13a062a6d4c1f719e0ad5333
julianakcm1 <54086293+julianakcm1@users.noreply.github.com>
2020-02-07 22:35:49 -0300

    Novo menu, mas sem nome do arquivo
    O antigo estava compactando e descompactando no comando "Compactar"

--- Huff da ju (com o menu)/menu.c
+++ Huff da ju (com o menu)/menu.c
@@ -6,76 +6,78 @@
 
 void menu()
 {
-    printf("\n====================================\n");
+    printf("\n=============================\n");
     printf("\n\tHUFFMAN\n");
-    printf("\n====================================\n");
+    printf("\n=============================\n");
 
     while(1)
     {
-        int choice, i;
+        int choice;
         printf("\nEscolha uma opção:\n\n1 >> Compress\n\n2 >> Descompress\n\n3 >> Exit\n\n");
     
         scanf("%d", &choice);
 
-        char name[256];
+        //char name[200];
 
-        switch(choice)
+        if(choice == 1)
         {
-            case 1:
+            //printf("\nDigite o nome do arquivo:\n\n");
+
+            //scanf("%s", name);
+
+            //FILE* file = fopen(name, "rb");
+
+            if(file == NULL)
             {
-                //printf("Digite o nome do arquivo:\n\n");
+                printf("Empty file\n");
+                break;
+            }
+            
+            lli frequence[256] = {0};
+            fill_freq_array(frequence);
 
-                //scanf("%s", name);
-
-                //FILE* file = fopen(name, "rb");
-
-                // if(file == NULL)
-                // {
-                //     printf("Empty file\n");
-                //     break;
-                // }
-                 
-                
-                    lli frequence[256] = {0};
-                    fill_freq_array(frequence);
-
-                    PRIORITY_QUEUE* queue = create_priority_queue();
-                    fill_priority_queue(frequence, queue);
+            PRIORITY_QUEUE* queue = create_priority_queue();
+            fill_priority_queue(frequence, queue);
                     //print_queue(queue->head);
 
-                    NODE* tree = create_huff_tree(queue);
+            NODE* tree = create_huff_tree(queue);
                     //print_tree(tree);
 
-                    HASH* hash = create_hash();
-                    new_codification(hash, tree, 0, 0); 
+            HASH* hash = create_hash();
+            new_codification(hash, tree, 0, 0); 
                     
                     //print_hash(hash);
 
-                    FILE* compacted_file = fopen("video.mp4.huff", "wb"); // arquivo de escrita compactado.
+            FILE* compacted_file = fopen("video.mp4.huff", "wb"); // arquivo de escrita compactado.
                     
-                    uchar trash = get_trash(hash, frequence); // trash so ocupa 3 bits, por isso so alocamos 1 byte para guardar o lixo.
-                    ushort size_tree = get_size_tree(tree);
+            uchar trash = get_trash(hash, frequence); // trash so ocupa 3 bits, por isso so alocamos 1 byte para guardar o lixo.
+            ushort size_tree = get_size_tree(tree);
                     //printf("lixo %d\n arvore %u\n", trash, size_tree);
 
-                    ushort header = create_file_header(hash, frequence, tree, trash, size_tree);
-                    write_header(header, compacted_file); // escrever o cabeçalho no arquivo compactado.
+            ushort header = create_file_header(hash, frequence, tree, trash, size_tree);
+            write_header(header, compacted_file); // escrever o cabeçalho no arquivo compactado.
                     
-                    get_pre_order_tree(tree, compacted_file);
-                    compact_file(compacted_file, hash, trash);
+            get_pre_order_tree(tree, compacted_file);
+            compact_file(compacted_file, hash, trash);
 
-                    fclose(compacted_file);
-                
-            }
+            printf("\n\nO arquivo foi compactado com sucesso!\n\n");
 
-            case 2:
-            {
-                descompact();
-            }
+            fclose(compacted_file);
+        }
 
-            case 3:
-            {
-                return;
-            } 
+        else if(choice == 2)
+        {
+            descompact();
+            printf("\n\nO arquivo foi descompactado com sucesso!\n\n");
+            break;
+        }
+
+        else if(choice == 3) return;
+        
+        else
+        {
+            printf("\n\nOperação inválida. Tente novamente.\n\n");
+            return;
         }
     }
     return;   
